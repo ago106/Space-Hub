164d0232d52153a2d603fca81d380952ac8982e2
ago106 <168357108+ago106@users.noreply.github.com>
2025-10-22 14:02:26 +0700

    Add files via upload

---
+++ LuaCore/Additional/Status.lua
@@ -0,0 +1,13 @@
+local Status = "Active" -- Active/Maintenance
+local DiscordLink = "discord.gg/ktC6dNVxDC"
+
+print("[Space Hub]: " .. Status)
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+if Status == "Maintenance" then
+    local kickMessage = string.format("Space Hub is currently under maintenance.\nPlease check back later.\n\n%s", DiscordLink)
+    LocalPlayer:Kick(kickMessage)
+    return
+end
