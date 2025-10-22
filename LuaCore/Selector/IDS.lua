f50d87678b504bec26dbba1555b7965c68f11b44
ago106 <168357108+ago106@users.noreply.github.com>
2025-10-22 17:54:21 +0700

    Create IDS.lua

---
+++ LuaCore/Selector/IDS.lua
@@ -0,0 +1,46 @@
+print("[Space Hub]: Loaded IDs list of scripts.")
+local IDS = {
+    [537413528]       = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BABFT.lua",                    -- BABFT
+    [85896571713843]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BGSI.lua",                     -- Bubble gum simulator infinity
+    [109652885385286] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Backyard.lua",                 -- Dig The backyard
+    [78041891124723]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BloodDebt.lua",                -- Blood Debt
+    [101949297449238] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BuildIsland.lua",              -- Build an Island
+    [70876832253163]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/DeadRailsBond.lua",            -- Dead Rails bond farm
+    [87700573492940]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Drill.lua",                    -- Untilted drill game
+    [126884695634066] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Garden.lua",                   -- Grow a Garden
+    [138878871969281] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Skinwalkers.lua",              -- lobby/game
+    [94725155944518]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Skinwalkers.lua",              -- lobby/game
+    [1537690962]      = "",                                                                                                       -- BSS 
+    [142823291]       = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/MM2.lua",                      -- MM2
+    [335132309]       = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/MM2.lua",                      -- MM2
+    [16732694052]     = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Fisch.lua",                    -- Fisch
+    --[16732694052]   = "https://gist.githubusercontent.com/ago106/7c61de715186c68b7e0580b4e7b1f8a0/raw/",                        -- Fisch old | No Key | Bannable v2.1
+    [131716211654599] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Fisch.lua",                    -- Fisch sea 1
+    [18901165922]     = "https://gist.githubusercontent.com/ago106/5a42b815cccc68081f568a898ef55aab/raw",                         -- Pets Go | No key
+    [126244816328678] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/DIG.lua",                      -- DIG
+    [99567941238278]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/InkGame.lua",                  -- Ink Game Lobby
+    [125009265613167] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/InkGame.lua",                  -- Ink Game Game
+    [109983668079237] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/StealBrainrot",                -- Steal A Brainrot
+    [96342491571673]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/StealBrainrot",                -- Steal A Brainrot TutorialS
+    [18687417158]     = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Forsaken.lua",                 -- Forsaken
+    [132073733865215] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Dep%20Simulator%202",          -- Dep Simulator 2
+    [79546208627805]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/99DaysInForest.lua",           -- 99 Days In Forest | Lobby
+    [126509999114328] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/99DaysInForest.lua",           -- 99 Days In Forest | Game
+    [89343390950953]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/MySingingBrainrot.lua",        -- My Singing Brainrot
+    [110931811137535] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/CatchAndFeedBrainrot.lua",     -- Catch And Feed Brainrot
+    [111989938562194] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BrainrotEvolution.lua",        -- Brainrot Evolution
+    [11276071411]     = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/NpcOrDie.lua",                 -- NpsOrDie
+    [77483797026305]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/SFOTH.lua",                    -- Sword Fights on the Heights IV: Reforged
+    [129827112113663] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Prospecting.lua",              -- Prospecting
+    [93978595733734]  = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/ViolenceDistruct.lua",         -- Violence Distruct
+    [185655149]       = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Bloxburg.lua",                 -- Bloxburg
+    [119048529960596] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/RS3.lua",                      -- Restaurant Tycoon 3
+    [127742093697776] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/PlantVsBrainrots.lua",         -- Plant Vs Brainrots
+    [121864768012064] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/FishIt.lua",                   -- Fish It 
+    [138837502355157] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Grace.lua",                    -- Grace | Lobby
+    [110333320616502] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Grace.lua",                    -- Grace | Game
+    [105555311806207] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BuildAZoo.Lua",                -- Build A Zoo
+    --[] = "",
+}
+
+return IDS
