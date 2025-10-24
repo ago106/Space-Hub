115a2921865d47946239b5ab3561efc920be3e68
ago106 <168357108+ago106@users.noreply.github.com>
2025-10-24 19:19:48 +0700

    Update SaveManager.lua

--- LuaCore/Library/Linoria/SaveManager.lua
+++ LuaCore/Library/Linoria/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-    SaveManager.Folder = 'LinoriaLibSettings'
+    SaveManager.Folder = 'Space-Hub'
     SaveManager.Ignore = {}
     SaveManager.LoadedConfigs = {}; -- Track loaded configs to prevent double execution
     SaveManager.Parser = {
