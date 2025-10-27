ad5008a862a1d9e9e9c15b5267101f82c5ca59f0
ago106 <168357108+ago106@users.noreply.github.com>
2025-10-27 18:24:13 +0700

    Update Freemium.lua

--- LuaCore/Premium/Freemium.lua
+++ LuaCore/Premium/Freemium.lua
@@ -1,15 +1,4 @@
-local Event = "31.10"
+local Event = "25.11"
 
-function checkDate(eventStr)
-    if eventStr == "none" then return false end
-    
-    local day, month = eventStr:match("(%d+)%.(%d+)")
-    local current = os.date("*t")
-    
-    local eventTime = os.time{year=current.year, month=month, day=day}
-    local novemberFirst = os.time{year=current.year, month=11, day=1}
-    
-    return os.time() >= eventTime and os.time() < novemberFirst
-end
-
-return Event ~= "none" and checkDate(Event)
+local d,m = Event:match("(%d+)%.(%d+)")
+return os.time() <= os.time{year=os.date("*t").year, month=m, day=d}
